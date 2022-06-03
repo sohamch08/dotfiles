de3624f3fd7cb4cd20e02bdd4dbf499b92dfd58d
lambdasolver <sohamchatterjee999@gmail.com>
2022-06-03 14:54:03 +0530

    screenshotselect

--- bin/screenshotselect.sh
+++ bin/screenshotselect.sh
@@ -5,9 +5,6 @@ name=$(date +"%d-%m-%y-%T")
 file_name=$name.png
 
 
-scrot -s ~/Pictures/$file_name
+maim -s -f png -o ~/Pictures/$file_name
 
 xclip -selection clipboard -target image/png -i ~/Pictures/$file_name
-
-
-
