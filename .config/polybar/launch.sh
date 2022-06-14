4d6be770ed505a2896461ec77be411aa46e2e8be
lambdasolver <sohamchatterjee999@gmail.com>
2022-06-15 00:57:04 +0530

    git-configs

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -4,9 +4,9 @@
 killall -q polybar
 
 # Wait until the processes have been shut down
-polybar main 2>&1 | tee -a /tmp/polybar.log & disown
-# polybar top 2>&1 | tee -a /tmp/polybar.log & disown &
-# polybar bottom 2>&1 | tee -a /tmp/polybar.log & disown
+# polybar main 2>&1 | tee -a /tmp/polybar.log & disown
+polybar top 2>&1 | tee -a /tmp/polybar.log & disown &
+polybar bottom 2>&1 | tee -a /tmp/polybar.log & disown
 
 # Launch
 # polybar main &
