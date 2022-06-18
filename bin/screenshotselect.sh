13fde6a7afb21fc6624fc99ac43077092409f312
lambdasolver <sohamchatterjee999@gmail.com>
2022-06-19 01:33:03 +0530

    file name modified and notify send added

--- bin/screenshotselect.sh
+++ bin/screenshotselect.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 
-name=$(date '+%Y-%m-%d %H:%M:%S')
+name=$(date '+%Y-%m-%d-%H:%M:%S')
 file_name=$name.png
 
 
 maim -s -f png -o ~/Pictures/$file_name
 
 xclip -selection clipboard -target image/png -i ~/Pictures/$file_name
+
+notify-send "Screenshot taken ${file_name}"
