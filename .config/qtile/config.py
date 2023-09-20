754a0683def38b9b4461352a7d48c85f4e234ad1
sohamch08 <sohamchatterjee999@gmail.com>
2023-09-21 01:41:46 +0530

    updated

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -146,7 +146,7 @@ extension_defaults = widget_defaults.copy()
 
 screens = [
     Screen(
-        bottom=bar.Bar(
+        top=bar.Bar(
             [
                 widget.CurrentLayout(),
                 widget.GroupBox(),
