ff8f83f22f27ff350977386f714555cb714b6cb6
sohamch08 <sohamchatterjee999@gmail.com>
2023-10-15 04:03:01 +0530

    updated

---
+++ setup.sh
@@ -0,0 +1,56 @@
+# Setup bashrc
+if [[ -e "$HOME/.bashrc" ]]; then
+    mv $HOME/.bashrc $HOME/.bashrc.bak
+    ln -s $(pwd)/.bashrc $HOME/.bashrc
+else
+    ln -s $(pwd)/.bashrc $HOME/.bashrc
+fi
+
+# Setup starship prompt
+if [[ -e "$HOME/.config/starship.toml" ]]; then
+    mv $HOME/.config/starship.toml $HOME/.config/starship.toml.bak
+    ln -s $(pwd)/.config/starship.toml $HOME/.config/starship.toml
+else
+    ln -s $(pwd)/.config/starship.toml $HOME/.config/starship.toml
+fi
+# Setup cmus
+if [ -d "$HOME/.config/cmus" ]; then
+    mv $HOME/.config/cmus $HOME/.config/cmus.bak
+    ln -s $(pwd)/.config/cmus $HOME/.config/cmus
+else 
+    ln -s $(pwd)/.config/cmus $HOME/.config/cmus
+fi
+
+# Setup neofetch
+if [ -d "$HOME/.config/neofetch" ]; then
+    mv $HOME/.config/neofetch $HOME/.config/neofetch.bak
+    ln -s $(pwd)/.config/neofetch $HOME/.config/neofetch
+else 
+    ln -s $(pwd)/.config/neofetch $HOME/.config/neofetch
+fi
+
+# Setup nvim
+if [ -d "$HOME/.config/nvim" ]; then
+    mv $HOME/.config/nvim $HOME/.config/nvim.bak
+    ln -s $(pwd)/.config/nvim $HOME/.config/nvim
+else 
+    ln -s $(pwd)/.config/nvim $HOME/.config/nvim
+fi
+
+
+# Setup rofi
+if [ -d "$HOME/.config/rofi" ]; then
+    mv $HOME/.config/rofi $HOME/.config/rofi.bak
+    ln -s $(pwd)/.config/rofi $HOME/.config/rofi
+else 
+    ln -s $(pwd)/.config/rofi $HOME/.config/rofi
+fi
+
+# Setup bin
+if [ -d "$HOME/bin" ]; then
+    mv $HOME/bin $HOME/bin.bak
+    ln -s $(pwd)/bin $HOME/bin
+else 
+    ln -s $(pwd)/bin $HOME/bin
+fi
+
---
+++ ytdownload.py
@@ -0,0 +1,63 @@
+import subprocess as sb
+import os
+
+#global shit
+app = "youtube-dl"
+path = "-o '$HOME/Downloads/youtube-dl_songs/%(channel)s/%(title)s - %(channel)s.%(ext)s'"
+title = "-f 140 -o '%(title)s - %(channel)s.%(ext)s'"
+audiodownload = "-f 140 --extract-audio --audio-format mp3 --audio-quality 0"
+thumbnaildownload = "--write-thumbnail --skip-download"
+filename = "--get-filename"
+#function to check if name is present or not
+def check_name(name):
+    names = []
+    #load names form the history file
+    with open("/home/sohamch/yt-download_history.txt",'r') as f:
+        for line in f.readlines():
+            l = line.strip("\n")
+            names.append(l)
+    f.close()
+    if name in names:
+        return True
+    else:
+        return False
+
+#generate bash file
+def generate_bash():
+    with open("/home/sohamch/command.sh","w") as f:
+        f.write(f"{app} {path} {audiodownload} {link}\n")
+        f.write(f"{app} {path} {thumbnaildownload} {link}\n")
+        f.write(f"{app} {title} {link} {filename} >> $HOME/yt-download_history.txt\n")
+        f.write("find ~/Downloads/youtube-dl_songs -name '*.webp' -exec magick {} {}.png \; -exec rm {} \;\n")
+        f.write("sort -o $HOME/yt-download_history.txt $HOME/yt-download_history.txt")
+    f.close()
+
+
+
+link = input()
+
+#get the video name
+command = f"{app} {title} {link} {filename}"
+
+name = sb.check_output(command,shell=True).decode().strip("\n")
+if check_name(name):
+    print("[*] Song is already Downloaded\n[*] Want to download again: ")
+    x = input()
+    if x == "y" or x =="Y":
+        #store data in command.sh file and execute it
+        generate_bash()
+        os.system("/home/sohamch/command.sh")
+    else:
+        exit(1)
+else:
+    generate_bash()
+    os.system("/home/sohamch/command.sh")
+
+
+
+# with open('command.sh','w') as f:
+#     f.write(f"{app} {title} {filename} {link} | python check.py\n")
+#     f.write(f"{app} {path} {audiodownload} {link}\n")
+#     f.write(f"{app} {path} {thumbnaildownload} {link}\n")
+#     f.write(f"{app} {title} {filename} {link}  >> yt-download_history.txt")
+   
