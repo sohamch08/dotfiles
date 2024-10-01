92137f7f70fda485b40ef3a52084e41300b31f47
sohamch08 <sohamchatterjee999@gmail.com>
2024-10-02 00:54:08 +0530

    updated

--- bin/batterynotify.sh
+++ bin/batterynotify.sh
@@ -3,9 +3,9 @@
 while true; do
     battery_level=$(acpi -b | grep -P -o '[0-9]+(?=%)')
 
-    if [[ $battery_level -le 15 ]]; then
+    if [[ $battery_level -le 30 ]]; then
         dunstify -u critical 'Low Battery'$'\n''Battery level is '$battery_level'%' -i "/home/sohamch/.config/dunst/icons/battery-alert.png"
-    elif [[ $battery_level -ge 90 ]]; then
+    elif [[ $battery_level -ge 80 ]]; then
         dunstify -u low 'Full Battery'$'\n''Battery level is '$battery_level'%' -i "/home/sohamch/.config/dunst/icons/battery-alert-variant.png"
     fi
 
